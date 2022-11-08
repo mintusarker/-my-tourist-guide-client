b2a816ba3098a9fadcb3bfe9640e2377b2eccc3a
mintusarker <sarkermintu707@gmail.com>
2022-11-08 13:59:56 +0600

    login form done

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,20 +1,49 @@
-import React, { Children, createContext, useState } from 'react';
+import React, { Children, createContext, useEffect, useState } from 'react';
 import app from '../Firebase/Firebase.config';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
 
     const createUser = (email, password) =>{
-        return createUserWithEmailAndPassword(auth, email, password)
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+
+    const login = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logOut = () => {
+        return signOut(auth);
     }
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+              console.log(currentUser)
+              setUser(currentUser);
+              setLoading(false);
+          })
+          return () => {
+             return unsubscribe();
+          }
+      }, [])
+
     const authInfo = {
          user,
-         createUser
+         createUser,
+         login,
+         logOut,
+         loading,
     }
     return (
         <div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const { login, setLoading } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            if(user){
+                navigate(from, {replace: true});
+            }
+            setError('')
+        })
+        .catch(error => {
+            console.error(error);
+            setError(error.message);
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+    };
+
+
     return (
         <div className="hero w-full my-5">
         <div className="hero-content flex-col">
             <div className="card flex-shrink-0 w-full shadow-2xl py-20">
                 <h1 className="text-5xl text-center text-blue-500 font-semibold">Login</h1>
-                <form className="card-body">
+                <form onSubmit={handleLogin} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -25,6 +58,7 @@ const Login = () => {
                     </div>
                     <div className="form-control mt-6">
                         <input className="btn btn-primary w-96" type="submit" value="Login" />
+                        <p className='text-red-500'>{error}</p>
                     </div>
                     <div className="form-control mt-6">
                         <input className="btn btn-primary w-96" type="submit" value="Login With Google" />
