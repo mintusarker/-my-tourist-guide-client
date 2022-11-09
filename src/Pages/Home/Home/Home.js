1f8e66adf07674c7f0027b6d18639130612d9044
mintusarker <sarkermintu707@gmail.com>
2022-11-09 13:58:48 +0600

    home page card section start

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import MorePlace from '../More/MorePlace';
+import Services from '../Service/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Services></Services>
             <MorePlace></MorePlace>
             <About></About>
         </div>
--- src/Pages/Home/Service/Services.js
+++ src/Pages/Home/Service/Services.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCardLimit from './ServiceCardLimit';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('Service.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
-        <div>
-            <h2>all service</h2>
+        <div className='bg-slate-100 py-10'>
+            <div className='text-center'>
+                <h2 className="text-4xl pt-8 font-bold font-serif">My service Area</h2>
+                <p className='text-xl font-serif py-2'>I try to my best for your travel time. No more Confusion, No more Hesitation. Let's Enjoy.</p>
+            </div>
+            <div className='grid gap-5 px-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service => <ServiceCardLimit
+                        key={service._id}
+                        service={service}
+                    ></ServiceCardLimit>)
+                }
+            </div>
+           <div className='text-center'> <button className="btn btn-active  btn-warning  btn-wide">See All</button></div>
+
         </div>
     );
 };
